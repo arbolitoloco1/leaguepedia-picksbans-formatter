d859e8167aa8799a15dcb419fc9f74c0dcaaad30
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2021-11-14 11:47:30 -0300

    Update picksbans-aftermatch.py

--- picksbans-aftermatch.py
+++ picksbans-aftermatch.py
@@ -21,9 +21,12 @@ for x in range(len(champids)):
     champnames.append(champname.lower())
     champidslower.append(champid.lower())
 
-PICKSBANS = """{{PicksAndBansS7|blueteam={t1} |redteam={t2}\n|team1score= |team2score= |winner=\n|blueban1={bb1} |red_ban1={rb1}\n|blueban2={bb2} |red_ban2={rb2}\n|blueban3={bb3} |red_ban3={rb3}\n
+PICKSBANS = """{{{{PicksAndBansS7|blueteam={t1} |redteam={t2}\n|team1score= |team2score= |winner=\n|blueban1={bb1} |red_ban1={rb1}\n|blueban2={bb2} |red_ban2={rb2}\n|blueban3={bb3} |red_ban3={rb3}\n
 |bluepick1={bp1} |bluerole1={bpo1}\n                                           |red_pick1={rp1} |red_role1={rpo1}\n                                           |red_pick2={rp2} |red_role2={rpo2}\n|bluepick2={bp2} |bluerole2={bpo2}\n|bluepick3={bp3} |bluerole3={bpo3}\n                                           |red_pick3={rp3} |red_role3={rpo3}\n
-|blueban4={bb4} |red_ban4={rb4}\n|blueban5={bb5} |red_ban5={rb5}\n                                           |red_pick4={rp4} |red_role4={rpo4}\n|bluepick4={bp4} |bluerole4={bpo4}\n|bluepick5={bp5} |bluerole5={bpo5}\n                                           |red_pick5={rp5} |red_role5={rpo5}\n|game1=yes}}"""
+|blueban4={bb4} |red_ban4={rb4}\n|blueban5={bb5} |red_ban5={rb5}\n                                           |red_pick4={rp4} |red_role4={rpo4}\n|bluepick4={bp4} |bluerole4={bpo4}\n|bluepick5={bp5} |bluerole5={bpo5}\n                                           |red_pick5={rp5} |red_role5={rpo5}\n"""
+
+PBWITHGAME1 = "|game1=yes}}"
+PBWITHOUTGAME1 = "}}"
 
 types = {
     "bp1": "1st Blue Pick",
@@ -53,9 +56,10 @@ response = site.api('cargoquery',
 )
 parsed = json.dumps(response)
 data = json.loads(parsed)
-if len(data["cargoquery"]) == 0:
+if not data["cargoquery"]:
     print("Not matches found!")
     os.system("pause")
+    exit()
 elif len(data["cargoquery"]) > 1:
     print("Multiple matches found!")
     gi = input("Enter the Game Id: ")
@@ -64,13 +68,14 @@ elif len(data["cargoquery"]) > 1:
 	    limit = "max",
 	    tables = "ScoreboardGames=SG",
 	    fields = "SG.Team1Bans, SG.Team2Bans, SG.Team1Picks, SG.Team2Picks",
-	    where = "SG.DateTime_UTC >= '{date} 00:00:00' AND SG.DateTime_UTC <= '{datedelta} 00:00:00' AND SG.Team1 = '{t1}' AND SG.Team2 = '{t2}' AND SG.GameId = '{gi}'".format(date = date, datedelta = datedelta, t1 = t1, t2 = t2, gi = gi)
+	    where = 'SG.DateTime_UTC >= "{date} 00:00:00" AND SG.DateTime_UTC <= "{datedelta} 00:00:00" AND SG.Team1 = "{t1}" AND SG.Team2 = "{t2}" AND SG.GameId = "{gi}"'.format(date = date, datedelta = datedelta, t1 = t1, t2 = t2, gi = gi)
     )
     parsed = json.dumps(response)
     data = json.loads(parsed)
     if len(data["cargoquery"]) != 1:
         print("Error while trying to parse match!")
         os.system("pause")
+        exit()
 res = data["cargoquery"][0]["title"]["Team1Bans"]
 t1bans = res.split(",")
 res2 = data["cargoquery"][0]["title"]["Team2Bans"]
@@ -118,7 +123,7 @@ pbs = {
 }
 
 if "None" in pbs.values():
-    print("\nCheck for ban order please!")
+    print("\nCHECK FOR BAN ORDER!")
 
 for x in (range(len(list))):
     type = list[x]
@@ -186,6 +191,15 @@ for x in range(len(typesred)):
 finaldata = PICKSBANS.format(t1 = t1, t2 = t2, bb1 = pbs.get("bb1"), bb2 = pbs.get("bb2"), bb3 = pbs.get("bb3"), bb4 = pbs.get("bb4"), bb5 = pbs.get("bb5"), rb1 = pbs.get("rb1"), rb2 = pbs.get("rb2"), rb3 = pbs.get("rb3"), rb4 = pbs.get("rb4"), rb5 = pbs.get("rb5"), 
 bp1 = pbs.get("bp1"), bp2 = pbs.get("bp2"), bp3 = pbs.get("bp3"), bp4 = pbs.get("bp4"), bp5 = pbs.get("bp5"), rp1 = pbs.get("rp1"), rp2 = pbs.get("rp2"), rp3 = pbs.get("rp3"), rp4 = pbs.get("rp4"), rp5 = pbs.get("rp5"), 
 bpo1 = pbs.get("bpo1"), bpo2 = pbs.get("bpo2"), bpo3 = pbs.get("bpo3"), bpo4 = pbs.get("bpo4"), bpo5 = pbs.get("bpo5"), rpo1 = pbs.get("rpo1"), rpo2 = pbs.get("rpo2"), rpo3 = pbs.get("rpo3"), rpo4 = pbs.get("rpo4"), rpo5 = pbs.get("rpo5"))
+
+game1 = input("\nIs this game 1? (Y/N): ")
+while game1.upper() not in  ("Y", "N"):
+    print("Choose Y or N!")
+    game1 = input("\nIs this game 1? (Y/N): ")
+if game1.upper() == "Y":
+    finaldata = finaldata + PBWITHGAME1
+elif game1.upper() == "N":
+    finaldata = finaldata + PBWITHOUTGAME1
 
 print("\n" + finaldata)
 
