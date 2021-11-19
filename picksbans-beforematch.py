ee121a9d475f7678ea0fa9fd15f4340a48753aab
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2021-11-18 23:58:01 -0300

    Update picksbans-beforematch.py

--- picksbans-beforematch.py
+++ picksbans-beforematch.py
@@ -7,21 +7,14 @@ currentVer = versions.json()[0]
 response = requests.get("http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/champion.json".format(str(currentVer)))
 
 champdata = response.json()["data"]
-champkeyslist = list(champdata.keys())
-champs = []
-keysforchamps = {}
-prettychamps = {}
+champs = {}
 
-for x in range(len(champdata)):
-    champkey = champkeyslist[x]
-    champname = champdata[champkey]["name"]
-    champid = champdata[champkey]["id"]
-    champkeyn = champdata[champkey]["key"]
-    champs.append(champname.lower())
-    champs.append(champid.lower())
-    keysforchamps[champname.lower()] = champkeyn
-    keysforchamps[champid.lower()] = champkeyn
-    prettychamps[champkeyn] = champname
+for key, value in champdata.items():
+    champname = value["name"]
+    champid = value["id"]
+    champkey = value["key"]
+    champs[champname.lower()] = [champkey, champname]
+    champs[champid.lower()] = [champkey, champname]
 
 PICKSBANS = """{{{{PicksAndBansS7|blueteam={t1} |redteam={t2}\n|team1score= |team2score= |winner=\n|blueban1={bb1} |red_ban1={rb1}\n|blueban2={bb2} |red_ban2={rb2}\n|blueban3={bb3} |red_ban3={rb3}\n
 |bluepick1={bp1} |bluerole1={bpo1}\n                                           |red_pick1={rp1} |red_role1={rpo1}\n                                           |red_pick2={rp2} |red_role2={rpo2}\n|bluepick2={bp2} |bluerole2={bpo2}\n|bluepick3={bp3} |bluerole3={bpo3}\n                                           |red_pick3={rp3} |red_role3={rpo3}\n
@@ -31,26 +24,26 @@ PBWITHGAME1 = "|game1=yes}}"
 PBWITHOUTGAME1 = "}}"
 
 types = {
-    "bb1": "1st Blue Ban",
-    "rb1": "1st Red Ban",
-    "bb2": "2nd Blue Ban",
-    "rb2": "2nd Red Ban",
-    "bb3": "3rd Blue Ban",
-    "rb3": "3rd Red Ban",
     "bp1": "1st Blue Pick",
-    "rp1": "1st Red Pick",
-    "rp2": "2nd Red Pick",
     "bp2": "2nd Blue Pick",
     "bp3": "3rd Blue Pick",
-    "rp3": "3rd Red Pick",
-    "rb4": "4th Red Ban",
-    "bb4": "4th Blue Ban",
-    "rb5": "5th Red Ban",
-    "bb5": "5th Blue Ban",
-    "rp4": "4th Red Pick",
     "bp4": "4th Blue Pick",
     "bp5": "5th Blue Pick",
-    "rp5": "5th Red Pick"
+    "rp1": "1st Red Pick",
+    "rp2": "2nd Red Pick",
+    "rp3": "3rd Red Pick",
+    "rp4": "4th Red Pick",
+    "rp5": "5th Red Pick",
+    "bb1": "1st Blue Ban",
+    "bb2": "2nd Blue Ban",
+    "bb3": "3rd Blue Ban",
+    "bb4": "4th Blue Ban",
+    "bb5": "5th Blue Ban",
+    "rb1": "1st Red Ban",
+    "rb2": "2nd Red Ban",
+    "rb3": "3rd Red Ban",
+    "rb4": "4th Red Ban",
+    "rb5": "5th Red Ban"
 }
 
 order = input("PB Order Type (1 or 2): ")
@@ -58,12 +51,11 @@ while str(order) not in ("1", "2"):
     print("Choose 1 or 2!")
     order = input("PB Order Type (1 or 2): ")
 if str(order) == "1":
-    list = ["bb1", "rb1", "bb2", "rb2", "bb3", "rb3", "bp1", "rp1", "rp2", "bp2", "bp3", "rp3", "rb4", "bb4", "rb5", "bb5", "rp4", "bp4", "bp5", "rp5"]
+    orderl = ["bb1", "rb1", "bb2", "rb2", "bb3", "rb3", "bp1", "rp1", "rp2", "bp2", "bp3", "rp3", "rb4", "bb4", "rb5", "bb5", "rp4", "bp4", "bp5", "rp5"]
 elif str(order) == "2":
-    list = ["bb1", "bb2", "bb3", "bb4", "bb5", "rb1", "rb2", "rb3", "rb4", "rb5", "bp1", "bp2", "bp3", "bp4", "bp5", "rp1", "rp2", "rp3", "rp4", "rp5"]
+    orderl = ["bb1", "bb2", "bb3", "bb4", "bb5", "rb1", "rb2", "rb3", "rb4", "rb5", "bp1", "bp2", "bp3", "bp4", "bp5", "rp1", "rp2", "rp3", "rp4", "rp5"]
 
 pbs = {}
-pbslist = []
 posblue = []
 posred = []
 blueteamchamps = []
@@ -72,29 +64,25 @@ redteamchamps = []
 t1 = input("Blue Team: ")
 t2 = input("Red Team: ")
 
-for x in (range(len(list))):
-    type = list[x]
+for type in orderl:
     inputstring = types.get(type)
     champ = input("{}: ".format(inputstring))
     if champ.lower().strip() == "none" and ("bb" in type or "rb" in type):
         pbs[type] = "None"
+        pbs[type + "k"] = 0
         continue
-    key = keysforchamps.get(champ.lower().strip())
-    while (champ.lower().strip() not in champs or key in pbslist or not key) and champ.lower().strip() != "none":
+    key = champs.get(champ.lower().strip())
+    while champ.lower().strip() not in list(champs.keys()) or key[0] in list(pbs.values()) or not key:
         print("Champ not found or already selected!")
         champ = input("{}: ".format(inputstring))
         if champ.lower().strip() == "none" and ("bb" in type or "rb" in type):
             pbs[type] = "None"
+            pbs[type + "k"] = 0
             continue
-        key = keysforchamps.get(champ.lower().strip())
-    prettychamp = prettychamps.get(key)
+        key = champs.get(champ.lower().strip())
+    prettychamp = key[1]
     pbs[type] = prettychamp
-    pbslist.append(key)
-    if "bp" in type:
-        blueteamchamps.append(prettychamp)
-    elif "rp" in type:
-        redteamchamps.append(prettychamp)
-    continue
+    pbs[type + "k"] = key[0]
 
 typesblue = ["bpo1", "bpo2", "bpo3", "bpo4", "bpo5"]
 typesred = ["rpo1", "rpo2", "rpo3", "rpo4", "rpo5"]
@@ -103,18 +91,18 @@ print("\nAccepted roles: t, j, m, b, s")
 
 print("\nBLUE TEAM POSITIONS")
 
-for x in range(len(typesblue)):
-    type = typesblue[x]
-    inputstring = blueteamchamps[x]
+for x, type in enumerate(typesblue):
+    inputstring = pbs.get(list(types.keys())[x])
     while True:
-        champ = input("Position/Role for {}: ".format(inputstring))
-        if champ == "t" or champ == "j" or champ == "m" or champ == "b" or champ == "s":
-            if champ in posblue:
+        role = input("Position/Role for {}: ".format(inputstring))
+        role = role.lower().strip()
+        if role == "t" or role == "j" or role == "m" or role == "b" or role == "s":
+            if role in posblue:
                 print("The position has already been chosen for this team!")
                 continue
             else:
-                pbs[type] = champ
-                posblue.append(champ)
+                pbs[type] = role
+                posblue.append(role)
                 break
         else:
             print("The position is not valid!")
@@ -122,18 +110,19 @@ for x in range(len(typesblue)):
 
 print("\nRED TEAM POSITIONS")
 
-for x in range(len(typesred)):
-    type = typesred[x]
-    inputstring = redteamchamps[x]
+for x, type in enumerate(typesred):
+    x += 5
+    inputstring = pbs.get(list(types.keys())[x])
     while True:
-        champ = input("Position/Role for {}: ".format(inputstring))
-        if champ == "t" or champ == "j" or champ == "m" or champ == "b" or champ == "s":
-            if champ in posred:
+        role = input("Position/Role for {}: ".format(inputstring))
+        role = role.lower().strip()
+        if role == "t" or role == "j" or role == "m" or role == "b" or role == "s":
+            if role in posred:
                 print("The position has already been chosen for this team!")
                 continue
             else:
-                pbs[type] = champ
-                posred.append(champ)
+                pbs[type] = role
+                posred.append(role)
                 break
         else:
             print("The position is not valid!")
